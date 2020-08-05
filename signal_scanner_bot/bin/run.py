528d76451ef1ac72281842969a1e1413f0e5de57
Matthew Bowden <bowdenm@spu.edu>
2020-08-04 22:17:13 -0700

    Move message processing to separate module, add more filter rules

--- signal_scanner_bot/bin/run.py
+++ signal_scanner_bot/bin/run.py
@@ -2,11 +2,13 @@ import logging
 
 from signal_scanner_bot.signal_scanner_bot import listen_and_print
 
+log = logging.getLogger(__name__)
+
 
 if __name__ == "__main__":
     logging.basicConfig(
         format="[%(asctime)s - %(name)s - %(lineno)3d][%(levelname)s] %(message)s",
-        level=logging.INFO,
+        level=logging.DEBUG,
     )
     log.info("Listening...")
     listen_and_print()
---
+++ signal_scanner_bot/messages.py
@@ -0,0 +1,35 @@
+import logging
+from datetime import datetime
+from typing import Dict
+
+from . import env
+
+log = logging.getLogger(__name__)
+
+
+def process_message(blob: Dict) -> None:
+    log.debug(f"Got message: {blob}")
+    envelope = blob.get("envelope", {})
+    if not envelope or "dataMessage" not in envelope:
+        log.error(f"Malformed message: {blob}")
+
+    data = envelope["dataMessage"]
+    if any(
+        [
+            # No actual message contents
+            (data is None or not data.get("message")),
+            # If listen group is defined and there's not group info
+            (env.LISTEN_GROUP and "groupInfo" not in data),
+            # Listen group is defined but ID doesn't match
+            (
+                "groupInfo" in data
+                and env.LISTEN_GROUP
+                and data["groupInfo"].get("groupId") != env.LISTEN_GROUP
+            ),
+        ]
+    ):
+        return
+
+    message = data["message"]
+    timestamp = datetime.fromtimestamp(data["timestamp"] / 1000.0)
+    log.info(f"{timestamp.isoformat()}: '{message}'")
