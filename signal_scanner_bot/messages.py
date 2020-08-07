6536c449a1720a328690d14077b4dbd88136d683
Matthew Bowden <bowdenm@spu.edu>
2020-08-06 21:39:14 -0700

    Integrate tweeting with signal message processing

--- signal_scanner_bot/messages.py
+++ signal_scanner_bot/messages.py
@@ -1,12 +1,12 @@
 import logging
-from typing import Dict
+from typing import Dict, Optional
 
 from .filters import FILTERS, message_timestamp
 
 log = logging.getLogger(__name__)
 
 
-def process_message(blob: Dict) -> None:
+def process_message(blob: Dict) -> Optional[str]:
     log.debug(f"Got message: {blob}")
     envelope = blob.get("envelope", {})
     if not envelope or "dataMessage" not in envelope:
@@ -20,3 +20,4 @@ def process_message(blob: Dict) -> None:
     message = data["message"]
     timestamp = message_timestamp(data)
     log.info(f"{timestamp.isoformat()}: '{message}'")
+    return f"{message} @ {timestamp.strftime('%l:%M:%S%p')}"
