e65c5c677d737719e03c8e353daddc7302c6e9be
Matthew Bowden <bowdenm@spu.edu>
2020-08-07 19:49:30 -0700

    Update run script

--- signal_scanner_bot/bin/run.py
+++ signal_scanner_bot/bin/run.py
@@ -1,14 +1,25 @@
 import logging
 
+import click
+
 from signal_scanner_bot.signal_scanner_bot import listen_and_print
 
 log = logging.getLogger(__name__)
 
+logging.basicConfig(
+    format="[%(asctime)s - %(name)s - %(lineno)3d][%(levelname)s] %(message)s",
+    level=logging.INFO,
+)
 
-if __name__ == "__main__":
-    logging.basicConfig(
-        format="[%(asctime)s - %(name)s - %(lineno)3d][%(levelname)s] %(message)s",
-        level=logging.INFO,
-    )
+
+@click.command()
+@click.option("-d", "--debug", is_flag=True)
+def cli(debug: bool) -> None:
+    if debug:
+        logging.getLogger().setLevel(logging.DEBUG)
     log.info("Listening...")
     listen_and_print()
+
+
+if __name__ == "__main__":
+    cli()
