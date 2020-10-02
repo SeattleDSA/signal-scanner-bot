d218ed4e16245942eed6dccc8fff9a2b5ea04151
Marcus Puckett <mpuckett259@gmail.com>
2020-10-01 19:17:16 -0700

    Implement long tweet serialization (#25)
    * Serializing long tweets
    
    Added functionality to send_tweet function in twitter.py to allow handling of long messages (greater than 260 characters, which is the max 280 minus a padding of 20 characters for metadata) by splitting them up over multiple tweets and adding an tweet number and total tweet thread length to threaded tweets.
    
    Additionally, the first tweet in a thread will be the only tweet with the hashtags and timestamp.
    
    Will need to test this commit in a real world scenario still. Just need to discuss with the maintainer.
    
    * Update Dockerfile/docker-compose.yml
    
    I added some more explicit commands for building the docker image out.
    
    Dockerfile:
    Removed the copy of the full build directory because Docker actually will just copy all the files recursively but put them directly in the target directory and not copy the actual folder structure. This should have completely messed up the build process but we'll see why it didn't in a second.
    
    Explicitly copied out needed files
    
    Swapped /app to . for the register-number.sh script because that's the current working dir anyway.
    
    docker-compose.yml:
    Mapped the signal_scanner_bot from the build dir to /app/signal_scanner_bot. The reason for this is because the build directory was being mounted like a volume and effectively over-writing everything that the Dockerfile was doing. This was the actual reason my builds were being weird and the reason the COPY scripts/register-number.sh . line wasn't really doing anything.
    
    * Final updates for tweet serialization
    
    Final fixes implemented for tweet serialization.
    
    * Update signal_scanner_bot/twitter.py
    
    Co-authored-by: Madison Bowden <bowdenm@spu.edu>
    
    * Update signal_scanner_bot/twitter.py
    
    Co-authored-by: Madison Bowden <bowdenm@spu.edu>
    
    * Update signal_scanner_bot/twitter.py
    
    Co-authored-by: Madison Bowden <bowdenm@spu.edu>
    
    * Update signal_scanner_bot/twitter.py
    
    Co-authored-by: Madison Bowden <bowdenm@spu.edu>
    
    * Update signal_scanner_bot/twitter.py
    
    Co-authored-by: Madison Bowden <bowdenm@spu.edu>
    
    * PR comment updates
    
    Removing some unnecessary files from the Dockerfile
    
    Adding proper typing in twitter.py script
    Breaking out complex tweet chunking logic into a separate function
    
    Fixing some style issues in addition to the above changes.
    
    * formatting update
    
    Final formatting fix
    
    * Final PR updates for real
    
    Resolving some flake8 vs black formatting issues....
    
    Co-authored-by: Madison Bowden <bowdenm@spu.edu>

--- signal_scanner_bot/twitter.py
+++ signal_scanner_bot/twitter.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import datetime
 from textwrap import dedent
+from typing import List
 
 import tweepy
 
@@ -15,6 +16,7 @@ log = logging.getLogger(__name__)
 SEND_HASHTAGS = ["#SeattleProtestComms", "#SeaScanner", "#SeattleProtests"]
 RECEIVE_HASHTAGS = ["#SeattleProtestComms"]
 TWEET_MAX_SIZE = 280
+TWEET_PADDING = 20
 
 
 ################################################################################
@@ -35,16 +37,82 @@ def get_api():
 ################################################################################
 def send_tweet(tweet: str, timestamp: datetime, api: tweepy.API) -> None:
     hashtags = " ".join(SEND_HASHTAGS)
-    if len(tweet + hashtags) >= 260:
-        # TODO: better
-        log.warning(f"Cannot tweet message, exceeds length: {tweet}")
-        return
 
-    formatted = dedent(
-        f"""
-    {tweet} @ {timestamp.strftime('%l:%M:%S%p').strip()}
+    # Check if tweet is longer than 280 minus a 20 char padding, add to list if
+    # not, continue with dividing up text if so
+    if len(tweet + hashtags) >= TWEET_MAX_SIZE - TWEET_PADDING:
+        tweets_list = create_tweet_thread(tweet, hashtags)
+    else:
+        tweets_list = [tweet]
 
-    {hashtags}
-    """
-    )
-    api.update_status(formatted)
+    # tweet_id used to track previous tweet in thread, set to None for first
+    # This iterates through all (even if just one) tweets in list to send
+    tweet_id = None
+    for index, sub_tweet in enumerate(tweets_list):
+
+        # If this is the first tweet add timestamp and hashtags
+        if index == 0:
+            formatted = dedent(
+                f"""
+            {timestamp.strftime('%H:%M:%S%p').strip()}
+
+            {sub_tweet}
+
+            {hashtags}
+            """
+            )
+        else:
+            formatted = sub_tweet
+
+        # If first tweet send without reply to tweet ID parameter, if part
+        # of a thread send reply using ID of last tweet sent.
+        if tweet_id is None:
+            status_obj = api.update_status(status=formatted)
+            tweet_id = status_obj.id
+        else:
+            status_obj = api.update_status(
+                status=formatted,
+                in_reply_to_status_id=tweet_id,
+                auto_populate_reply_metadata=True,
+            )
+            tweet_id = status_obj.id
+
+
+def create_tweet_thread(message: str, hashtags: str) -> List[str]:
+    # Split tweet into word list, create empty list to store serialized tweets, index to track last position
+    tweet_word_list = message.split(" ")
+    tweets_list: List[str] = []
+    base_index = 0
+
+    # For loop through word list and enumerate the index because we'll need
+    # it but don't actually care about the list value so send to null
+    for index in range(len(tweet_word_list)):
+
+        # Check if it is the first tweet, which will contain hashtags and
+        # timestamp. If not first it will only contain the text plus ellipses
+        # and for the final one drop the ellipses
+        if len(tweets_list) == 0:
+            sub_tweet = " ".join(tweet_word_list[base_index:index]) + " ..." + hashtags
+        elif index < len(tweet_word_list):
+            sub_tweet = " ".join(tweet_word_list[base_index:index]) + " ..."
+        elif index == len(tweet_word_list):
+            sub_tweet = " ".join(tweet_word_list[base_index:index])
+
+        # When length of tweet reaches >260 chars save to list and set
+        # base index for next tweet
+        if len(sub_tweet) > TWEET_MAX_SIZE - TWEET_PADDING:
+            last_index = index - 1
+            tweets_list.append(
+                " ".join(tweet_word_list[base_index:last_index]) + " ..."
+            )
+            base_index = index - 1
+
+    # Save the last tweet
+    tweets_list.append(" ".join(tweet_word_list[base_index:]))
+
+    # Append the tweet number / tweet thread length to end of tweet. Again
+    # don't actually care about the list value so sending to null.
+    for index in range(len(tweets_list)):
+        tweets_list[index] += f" {index + 1}/{len(tweets_list)}"
+
+    return tweets_list
