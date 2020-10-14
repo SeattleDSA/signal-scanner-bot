4a4bf28ca6d087694c9384d84502f919e1c45fe8
Madison Bowden <bowdenm@spu.edu>
2020-10-13 18:43:40 -0700

    Ignore status messages that don't match auto scan (#39)

--- signal_scanner_bot/messages.py
+++ signal_scanner_bot/messages.py
@@ -80,8 +80,9 @@ def process_signal_message(blob: Dict, api: API) -> None:
     # Check if twitter-to-signal should be on/off
     condensed = _condense_command(message)
     notice = env.STATE.update_listening_status(condensed)
-    log.info(notice)
-    signal.send_message(notice, env.LISTEN_CONTACT)
+    if notice:
+        log.info(notice)
+        signal.send_message(notice, env.LISTEN_CONTACT)
 
 
 def process_twitter_message(status: Status) -> None:
