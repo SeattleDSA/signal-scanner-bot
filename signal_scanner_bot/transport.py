63beea023487770ea2a523a5567ce0cba9992315
Marcus Puckett <mpuckett259@gmail.com>
2020-10-22 16:08:46 -0700

    .gitignore and black update
    Fixing a couple inconsequential things.

--- signal_scanner_bot/transport.py
+++ signal_scanner_bot/transport.py
@@ -54,7 +54,9 @@ async def twitter_to_queue():
             await loop.run_in_executor(pool, _twitter_to_queue)
             await asyncio.sleep(5)
         except Exception as err:
-            log.error("Exception occurred in the Twitter to Queue pipeline, halting process")
+            log.error(
+                "Exception occurred in the Twitter to Queue pipeline, halting process"
+            )
             log.exception(err)
             signal.panic(err)
             env.STATE.STOP_REQUESTED = True
